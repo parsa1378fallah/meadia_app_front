b3510dcd59a91fa870f2294d57c0a85c5070df84
parsa <parsaddbb@gmail.com>
2023-10-20 07:48:38 +0330

    fix address of reposirory

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/media_app/"
+  base : "/media_app_front/"
 })
