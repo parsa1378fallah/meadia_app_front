c4683efdd417800e07780705dfc1d4a04e32904f
parsa <parsaddbb@gmail.com>
2023-10-21 09:06:43 +0330

    change styles

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,9 +2,9 @@ import Feed from "../../components/feed/Feed.jsx";
 import RightBar from "../../components/rightbar/RightBar.jsx";
 function Home() {
   return (
-    <div className="flex flex-row">
-      <Feed classes={"basis-3/4"} />
-      <RightBar classes={"basis-1/4"} />
+    <div className="flex flex-row ">
+      <Feed classes={"basis-full sm:basis-3/4"} />
+      <RightBar classes={"hidden sm:flex sm:basis-1/4"} />
     </div>
   );
 }
